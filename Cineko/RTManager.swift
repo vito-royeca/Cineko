264c861f4b2c31e14dec81e78581f97c76a546ae
jovito-royeca <jovit.royeca@gmail.com>
2016-04-14 11:52:55 +0800

    Updated the Sync cocoapod.

---
+++ Cineko/RTManager.swift
@@ -0,0 +1,13 @@
+//
+//  RTManager.swift
+//  Cineko
+//
+//  Created by Jovit Royeca on 14/04/2016.
+//  Copyright © 2016 Jovito Royeca. All rights reserved.
+//
+
+import UIKit
+
+class RTManager: NSObject {
+
+}
